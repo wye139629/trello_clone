1a377d15aa7453f43b8315d1ecf7ebbf64793376
will <wye139629@gmail.com>
2022-01-19 14:49:09 +0800

    feat: setup logout menu button

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,11 +1,15 @@
-import { SearchForm } from './../shared/SearchForm'
 import tw from 'twin.macro'
+
 import { faChevronDown, faBell, faUser } from 'lib/fontawsome/icons'
-import { Icon } from 'components/shared'
+import { Icon, SearchForm } from 'components/shared'
+import { Menu, MenuList, MenuButton, MenuItem } from '@reach/menu-button'
+import { useAuth } from 'context/authContext'
 
 const Button = tw.button`rounded border-0 bg-transparent py-[6px] px-[10px] cursor-pointer text-white hover:bg-gray-200/30`
 
 function Navbar() {
+  const { logout } = useAuth()
+
   return (
     <nav css={tw`flex px-[6px] py-[8px] bg-black/50 text-white items-center`}>
       <div>
@@ -26,11 +30,21 @@ function Navbar() {
         <Button>
           <Icon name={faBell} size="lg" />
         </Button>
-        <button
-          css={tw`bg-cyan-500 rounded-full border-0 cursor-pointer w-[32px] h-[32px]`}
-        >
-          <Icon name={faUser} css={tw`text-lg text-sky-900`} />
-        </button>
+        <Menu>
+          <MenuButton>
+            <button
+              css={tw`bg-cyan-500 rounded-full border-0 cursor-pointer w-[32px] h-[32px]`}
+            >
+              <Icon name={faUser} css={tw`text-lg text-sky-900`} />
+            </button>
+          </MenuButton>
+          <MenuList>
+            <div css={tw`px-[10px] mb-[10px] relative`}>
+              <h4 css={tw`text-center border-b pb-[8px]`}>帳號</h4>
+            </div>
+            <MenuItem onSelect={logout}>登出</MenuItem>
+          </MenuList>
+        </Menu>
       </div>
     </nav>
   )
